d0256dfec2c218ca03d5a99b31f8768117d0dcb9
Yohan Choi <remember2019@gmail.com>
2020-11-18 21:23:38 +0000

    adding insert feature to BinarySearchTree

--- index.js
+++ index.js
@@ -12,7 +12,30 @@ class BinarySearchTree {
     }
 
     insert(value) {
+        if (this.root === null) {
+            this.root = new Node(value);
+            return;
+        }
 
+        let currentNode = this.root;
+
+        while (currentNode) {
+            let nextNode = null;
+
+            if (compareNumbers(currentNode.value, value) > 0) {
+                nextNode = currentNode.left;
+                if (currentNode.left === null) {
+                    currentNode.left = new Node(value);
+                }
+            } else if (compareNumbers(currentNode.value, value) < 0) {
+                nextNode = currentNode.right;
+                if (currentNode.right === null) {
+                    currentNode.right = new Node(value);
+                }
+            }
+            
+            currentNode = nextNode;
+        }
     }
 
     lookup(value) {
@@ -23,6 +46,28 @@ class BinarySearchTree {
 
     }
 }
+
+
+function compareNumbers(firstNum, secondNum) {
+    if (firstNum > secondNum) {
+        return 1;
+    }
+
+    if (firstNum < secondNum) {
+        return -1;
+    }
+
+    return 0;
+}
+
+function traverse(node) {
+    const tree = { value: node.value };
+    tree.left = node.left === null ? null : traverse(node.left);
+    tree.right = node.right === null ? null : traverse(node.right);
+
+    return tree;
+}
+
 
 const tree = new BinarySearchTree();
 
@@ -38,13 +83,6 @@ JSON.stringify(traverse(tree.root));
 
 
 //       9
-//     4   20
-// 1   6   15  20
+//   4       20
+// 1   6   15  170
 
-function traverse(node) {
-    const tree = { value: node.value };
-    tree.left = node.left === null ? null : traverse(node.left);
-    tree.right = node.right === null ? null : traverse(node.right);
-
-    return tree;
-}
