830e0632cc2d07a1fab9f4cddefa33914fe2e709
Yohan Choi <remember2019@gmail.com>
2020-11-18 20:16:24 +0000

    adding initial files for binary search tree

---
+++ index.js
@@ -0,0 +1,50 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+
+    }
+
+    lookup(value) {
+
+    }
+
+    remove(value) {
+
+    }
+}
+
+const tree = new BinarySearchTree();
+
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+
+JSON.stringify(traverse(tree.root));
+
+
+//       9
+//     4   20
+// 1   6   15  20
+
+function traverse(node) {
+    const tree = { value: node.value };
+    tree.left = node.left === null ? null : traverse(node.left);
+    tree.right = node.right === null ? null : traverse(node.right);
+
+    return tree;
+}
